ef0a7fd1a47c53c4388f86b0dcf15bad7d86b9b1
Kerim Güven <4362712+akgvn@users.noreply.github.com>
2024-04-16 00:25:36 +0300

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,8 +1,17 @@
+$PSNativeCommandUseErrorActionPreference = $true
+$ErrorActionPreference = 'Stop'
+
 # AIR SDK works:
 $docs = [Environment]::GetFolderPath("MyDocuments")
 $compc = Join-Path $docs "AIR_SDKS" "AIRSDK_50.2.4" "bin" "compc.bat"
 
-& $compc -source-path .\src -load-config .\_config\config.xml
+$src = Join-Path $PSScriptRoot "src"
+$cfgDir = Join-Path $PSScriptRoot "_config"
+$cfg = Join-Path $cfgDir "config.xml"
+$cfgDbg = Join-Path $cfgDir "config-debug.xml"
+
+& $compc -source-path $src -load-config $cfgDbg
+& $compc -source-path $src -load-config $cfg
 
 # The following (Flex SDK) fails with: _config\config.xml(41): Error: unknown configuration variable 'include-classes'
 # $compc = Join-Path $docs "Flex SDK" "apache-flex-sdk-4.16.1-bin" "bin" "mxmlc.bat"
