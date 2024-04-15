4f2f452fda62d1b3f7a7d10dbf147333c402b09e
Kerim Güven <4362712+akgvn@users.noreply.github.com>
2024-04-16 00:25:54 +0300

    Fix the keyboard example build

--- compile_sample_with_flex.ps1
+++ compile_sample_with_flex.ps1
@@ -1,3 +1,6 @@
+$PSNativeCommandUseErrorActionPreference = $true
+$ErrorActionPreference = 'Stop'
+
 # I don't know why I put Flex SDK in documents, but it is there.
 $docs = [Environment]::GetFolderPath("MyDocuments") 
 $mxmlc = Join-Path $docs "Flex SDK" "apache-flex-sdk-4.16.1-bin" "bin" "mxmlc.bat"
@@ -9,4 +12,4 @@ $kaospad = Join-Path "." "samples" "SiONKaosPad" "KaosPad.as"
 $keyboard = Join-Path "." "samples" "SiONKeyboard" "keyboard.mxml"
 $tenorion = Join-Path "." "samples" "SiONTenorion" "Tenorion.as"
 
-Start-Process -filepath $mxmlc -argumentList @("$kaospad") -NoNewWindow -Wait
+Start-Process -filepath $mxmlc -argumentList @("$keyboard", "-o", "new.swf") -NoNewWindow -Wait
